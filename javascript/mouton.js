ea7b6992178bc5969476da59a11d7452fff55ac2
morganNwt <metodik60@hotmail.fr>
2024-03-21 13:48:34 +0100

    PorteFolio

---
+++ javascript/mouton.js
@@ -0,0 +1,186 @@
+console.log('Je suis ici');
+// Déclaration des Variables et constantes
+let plateauDeJeu = [];
+let plateauGagnant = [];
+let dom_h1, dom_plateau;
+var aGagner = false;
+
+const NB_MOUTONS = 3;
+const TAILLE = NB_MOUTONS * 2 + 1;
+const MOUTON_BLANC = '>';
+const MOUTON_NOIR = '<';
+const ESPACE_VIDE = ' ';
+
+/*********************************************
+ *         APPEL DES FONCTIONS  *
+ *********************************************/
+// 1 - initialier le plateau de jeu
+initialierPlateau();
+// 2 - afficher le plateau de jeu
+afficherPlateauDeJeu();
+// 3 - Jouer un coup
+//  Après avoir fait un déplacement je retourne un booléen qui vérifie si le joueur à gagné.
+// la fonction jouUnCoup() est executé au clic sur la balise 
+jouerUnCoup();
+
+
+/*********************************************
+ *          DECLARATION DES FONCTIONS  *
+ *********************************************/
+
+/**
+ * Fonction initialiserPlateau() qui crée 2 plateaux de jeu.
+ * Le premier est initialiseé en position de départ
+ * Le second est initialisé en position gagnante.
+ * une fois ceci fait, je crée une balise div dans lequel il y aura  7 balise img
+ * enfin je l'ajoute à mon DOM après le titre h1
+ * 
+ * @returns void
+ */
+function initialierPlateau() {
+    // 1 - Création des plateaux de jeu
+    for( let i = 0; i < TAILLE; i++) {
+        if( i < NB_MOUTONS){
+            plateauDeJeu[i] = MOUTON_BLANC;
+        }
+        else if(i > NB_MOUTONS) {
+            plateauDeJeu[i] = MOUTON_NOIR;
+        }
+        else {
+            plateauDeJeu[i] = ESPACE_VIDE;
+        }
+    }
+
+console.log('plateauDeJeu : ' + plateauDeJeu);
+
+    //Création du plateau gagnant
+    for( let i = 0; i < TAILLE; i++) {
+        if( i < NB_MOUTONS){
+            plateauGagnant[i] =  MOUTON_NOIR;
+        }
+        else if(i > NB_MOUTONS) {
+            plateauGagnant[i] = MOUTON_BLANC;
+        }
+        else {
+            plateauGagnant[i] = ESPACE_VIDE;
+        }
+    }
+    console.log('plateauGagnant : ' + plateauGagnant);
+
+    // Manipulation du DOM pour afficher le plateau vierge (Visuellement)
+    dom_h1 = document.getElementById('titreH1');
+    dom_plateau = document.createElement('div');
+    dom_plateau.setAttribute('id', 'plateauDeJeu');
+    dom_h1.after(dom_plateau);
+}
+
+/**************************************************************
+ * Fonction afficherPlateauDeJeu() qui va crée les balises img.
+ * Celles-ci auront pour le backgound-image un mouton blanc ou noir
+ * ou couleur blanche
+ *****************************************************************/
+
+function afficherPlateauDeJeu() {
+    dom_plateau = document.getElementById('plateauDeJeu');
+    dom_h1 = document.getElementById('titreH1');
+
+    // Avant de remplir ( en HTML) les moutons dans le plateau de jeu.
+    // Je vérifie si ma balise a des enfants
+    // Si oui, je supprime le 1er enfant de la liste
+    while(dom_plateau.hasChildNodes()){
+        dom_plateau.removeChild(dom_plateau.children[0]);
+
+    }
+
+    for( let i = 0 ; i < TAILLE ; i++) {
+        // A chaque tour de boucle, je crée une balise img, je la personnalise, je l'ajoute 
+        // à la suite dans la div du plateauDeJeu (qui sera détruite à la fin de la boucle)
+        
+        let dom_img = document.createElement('img');
+        dom_img.setAttribute('class', 'image');
+        dom_img.setAttribute('onclick', 'jouerUnCoup('+ i +')');
+
+        //Ici, on vérifie quel mouton est présent dans le tableau JS pour savoir quel
+        //mouton afficher en tant que src='image/'.
+        switch (plateauDeJeu[i]) {
+
+            case MOUTON_BLANC : 
+                // faire des doubles back slash \\ pour faire les chemins
+                dom_img.setAttribute('src', '../images/sheep_w.png');
+                dom_img.setAttribute('alt', 'mouton blanc');
+                break;
+            
+            case MOUTON_NOIR: 
+                dom_img.setAttribute('src', '../images/sheep_b.png'); 
+                dom_img.setAttribute('alt', 'mouton noir');
+                break;    
+                default: 
+             break;    
+        }
+        dom_plateau.append(dom_img); 
+    } 
+} 
+
+
+/**
+ * Fonction jouerUnCoupp() qui vérifie si le déplacement est possible
+ * si oui,
+ * - modifier le tableau plateauDeJeu
+ * -  mettre à jour la valeur de l'attribut src.
+ * Le param index représente le numéro d'indice que j'utilise pour vérifier la validité du déplacement.
+ * @param {*} index 
+ */
+
+function jouerUnCoup(index){
+    if (plateauDeJeu[index] == MOUTON_BLANC) {
+        if (plateauDeJeu[index + 1] == ESPACE_VIDE) {
+            plateauDeJeu[index + 1] = MOUTON_BLANC; 
+            plateauDeJeu[index] = ESPACE_VIDE;
+    }
+    else if(plateauDeJeu[index + 1] == MOUTON_NOIR && plateauDeJeu[index + 2] == ESPACE_VIDE ) {
+            plateauDeJeu[index + 2] = MOUTON_BLANC;
+            plateauDeJeu[index] = ESPACE_VIDE;
+   }
+}      
+    else if(plateauDeJeu[index] == MOUTON_NOIR) {
+         if(plateauDeJeu[index - 1] == ESPACE_VIDE) {
+            plateauDeJeu[index - 1] = MOUTON_NOIR;
+            plateauDeJeu[index] = ESPACE_VIDE;
+        }
+       else if(plateauDeJeu[index - 1] == MOUTON_BLANC && plateauDeJeu[index - 2] == ESPACE_VIDE){
+            plateauDeJeu[index - 2] = MOUTON_NOIR;
+            plateauDeJeu[index] = ESPACE_VIDE;
+        }   
+        else{
+
+    }
+    }
+
+    console.log('plateauDeJeu: ' + plateauDeJeu);
+    afficherPlateauDeJeu();
+
+    aGagner = verifJeu();
+    if(aGagner == true) {
+        dom_h1.innerText = 'BRAVO, C\'est gagné !!!';
+    }
+}       
+
+/**
+ * Le temps passe vite ici documentation
+ * @returns // booleen
+ */
+function verifJeu(){
+    let compteur = 0;
+    for(let i = 0; i < TAILLE ; i++) {
+        if(plateauDeJeu[i] == plateauGagnant[i]){
+            compteur++;
+        }
+    }
+    if(compteur === TAILLE) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+ 
---
+++ javascript/pendu.js
@@ -0,0 +1,155 @@
+//console.log('Vous êtes ici');
+// création de variable global
+var dom_jeu;
+var motSecret = motAleatoire();
+var motVisible = []; // Tableau de caractères (mot) visible inGame
+var lettreChoisie = [];
+var mauvaisesReponses = 0;
+
+/****************************************************************************
+ * Fonction motAleatoire() va choisir un mot aléatoire
+ * dans le Dico.js et stocker le mot aléatoire dans var motSecret
+ * @returns aleatoire
+ *
+ ***************************************************************************/
+function  motAleatoire() {
+    let i = Math.floor(Math.random() * nombreDeMots);
+    let aleatoire = dictionnaire[i]
+
+
+    return aleatoire;
+}
+/***************************************************************************
+ * Fonction init() va initialiser le jeu du pendu et
+ * incrémenter le motVisible en affichant la première et dernière lettre
+ * puis cacher le reste du mot avec des underscores 
+ ***************************************************************************/
+function init() {
+    // afficher motSecret, je dois récupérer <span id="jeu"></span>
+    dom_jeu = document.getElementById('jeu');
+    
+
+    // Hydrate/insérer chaque caractère du motSecret dans le tableau motVisible
+    for (let i = 0; i < motSecret.length; i++) {
+        if (i == 0 || i == motSecret.length - 1) {
+            motVisible.push(motSecret.charAt(i));
+        } else {
+            motVisible.push("_");
+        }
+    }
+    document.getElementById("motAffiche").textContent = motVisible.join(' ');  
+}
+/*****************************************************************************
+ * Fonction choisirLettre() va permettre :
+ * - De choisir une lettre, vérifier si elle n'a pas déjà été choisie.
+ * - Vérifier si la lettre n'est pas dans le mot secret
+ * - Vérifier si le joueur à gagner
+ * - Désactiver le boutton correspondant à la lettre choisie
+ * - terminer la fonction après avoir gagné
+ * 
+ * @return alert
+ *******************************************************************************/
+function choisirLettre(lettre) {
+    console.log("Lettre choisie : " + lettre);
+    lettre = lettre.toUpperCase();
+
+    // Vérifier si la lettre a déjà été choisie
+    if (lettreChoisie.includes(lettre)) {
+        alert("Lettre déjà choisie. Choisissez une autre lettre.");
+        return;
+    }
+
+    lettreChoisie.push(lettre);
+
+    // Vérifier si la lettre est dans le mot secret
+    let lettreCorrecte = false;
+
+    for (const [i, char] of motSecret.split('').entries()) {
+        if (char === lettre) {
+            motVisible[i] = lettre;
+            lettreCorrecte = true;
+        }
+    }
+
+    document.getElementById("motAffiche").textContent = motVisible.join(' ');
+
+    // Vérifier si le joueur a gagné
+    if (!motVisible.includes("_")) {
+        alert("Félicitations ! Vous avez gagné !");
+        resetGame();
+        return; // Terminer la fonction après avoir gagné
+    }
+
+    // Vérifier si la lettre n'est pas dans le mot secret
+    if (!lettreCorrecte) {
+        // mettre à jour l'image du Pendu
+        mauvaisesReponses++;
+        updatePendu();  
+    }
+    // Désactiver le bouton correspondant à la lettre choisie et la griser
+    var boutonLettre = document.getElementById('bouton-' + lettre);
+    if (boutonLettre) {
+        boutonLettre.disabled = true;
+        boutonLettre.classList.add('lettre-desactive'); 
+    }
+}
+/*******************************************************************************
+ * Fonction updatePendu() va permettre de mettre à jour les images du pendu et la
+ * couleur du background en fonction des erreurs avec un compteur d'erreurs max 
+ * 
+ ********************************************************************************/
+function updatePendu() {
+    var nombreImage = lettreChoisie.length;
+    var cordeImage = document.getElementById('cordeImage');
+
+    // Limiter le nombre d'erreurs à 6
+    var erreursMax = 6;
+    var erreurAfficher = Math.min(nombreImage, erreursMax);
+
+    // Changer l'image du pendu en fonction du nombre d'erreurs
+    cordeImage.src = "images/p" + erreurAfficher + ".gif";
+    console.log(' erreur: ' + erreurAfficher);
+
+    // Appeler getColorForWrongAnswers pour obtenir la couleur de fond
+    var couleurFond = getColorForWrongAnswers(mauvaisesReponses);
+    document.body.style.backgroundColor = couleurFond;
+    
+    if (nombreImage === erreursMax) {
+        alert("Désolé, vous avez perdu. Le mot était : " + motSecret);
+        resetGame();
+    }
+}
+/*********************************************************************************
+ * Fonction getColorForWrongAnswers() permet de changer la couleur du background
+ * en fonction des erreurs
+ **********************************************************************************/
+function getColorForWrongAnswers(mauvaisesReponses) {  
+    if (mauvaisesReponses === 1) {
+        return "yellow";
+    } else if (mauvaisesReponses === 2) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+/***********************************************************************************
+ * Fonction resetGame() va permettre de remettre le jeu à zéro pour rejouer
+ ***********************************************************************************/
+function resetGame() {
+    mauvaisesReponses = 0;
+    motVisible = [];
+    lettreChoisie = [];
+    document.body.style.backgroundColor = " "; 
+    init();
+}
+// Appeler init() lorsque la page est chargée
+window.onload = init;
+
+
+
+    
+
+
+
+    
+
