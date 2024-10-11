861fa05af32ce3a0e98efc6e7a8473a08842b9c9
morganNwt <metodik60@hotmail.fr>
2024-10-11 15:53:33 +0200

    feat(header): maj

--- components/_header.php
+++ components/_header.php
@@ -1,10 +1,6 @@
-<?php
-    $basePath = ($_SERVER['SCRIPT_NAME'] == '/index.php') ? './' : '../';
-?>
-
 <nav class="nav-flex-2">
-    <a href="<?php echo $basePath; ?>index.php" class="lien_icone">
-        <img src="<?php echo $basePath; ?>images/logoM1.png" alt="Logo de NAWROT Morgan">
+    <a href="/index.php" class="lien_icone">
+        <img src="/images/logoM1.png" alt="Logo de NAWROT Morgan">
     </a>
     <?php
         // Vérifiez que vous n'êtes pas sur les pages de connexion ou d'inscription
@@ -19,20 +15,20 @@
     ?>
     <div class="nav-flex">
         <?php if (!isset($_SESSION['userId'])): ?>
-            <a href="<?php echo $basePath; ?>index.php" class="nav-bar">Accueil</a>
-            <a href="<?php echo $basePath; ?>view/a_propos.php" class="nav-bar">À propos</a>
-            <a href="<?php echo $basePath; ?>view/gallerie_photos.php" class="nav-bar">Gallerie Photos</a>
-            <a href="<?php echo $basePath; ?>index.php#jeux" class="nav-bar">Jeux</a>
-            <a href="<?php echo $basePath; ?>view/connexion.php" class="nav-bar">Connexion</a>
-            <a href="<?php echo $basePath; ?>view/inscription.php" class="nav-bar">Inscription</a>
+            <a href="/index.php" class="nav-bar">Accueil</a>
+            <a href="/view/a_propos.php" class="nav-bar">À propos</a>
+            <a href="/view/gallerie_photos.php" class="nav-bar">Gallerie Photos</a>
+            <a href="/index.php#jeux" class="nav-bar">Jeux</a>
+            <a href="/view/connexion.php" class="nav-bar">Connexion</a>
+            <a href="/view/inscription.php" class="nav-bar">Inscription</a>
         <?php else : ?>
-            <a href="<?php echo $basePath; ?>index.php" class="nav-bar">Accueil</a>
-            <a href="<?php echo $basePath; ?>view/a_propos.php" class="nav-bar">À propos</a>
-            <a href="<?php echo $basePath; ?>view/gallerie_photos.php" class="nav-bar">Gallerie Photos</a>
-            <a href="<?php echo $basePath; ?>index.php#jeux" class="nav-bar">Jeux</a>
-            <a href="<?php echo $basePath; ?>view/admin.php" class="nav-bar">Admin</a>
-            <a href="<?php echo $basePath; ?>services/db_deconnexion.php"> 
-            <img src="../images/Deco.jpg" alt="Image de rawpixel.com sur Freepik" class="nav-deconnexion"></a>
+            <a href="/index.php" class="nav-bar">Accueil</a>
+            <a href="/view/a_propos.php" class="nav-bar">À propos</a>
+            <a href="/view/gallerie_photos.php" class="nav-bar">Gallerie Photos</a>
+            <a href="/index.php#jeux" class="nav-bar">Jeux</a>
+            <a href="/view/admin.php" class="nav-bar">Admin</a>
+            <a href="/services/db_deconnexion.php"> 
+            <img src="/../images/Deco.jpg" alt="Image de rawpixel.com sur Freepik" class="nav-deconnexion"></a>
         <?php endif; ?>
     </div>
 </nav>
